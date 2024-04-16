beddf7dfd590d42a01cd1ab13b1da82c9855910e
John <john.thom.gray@gmail.com>
2024-04-16 16:06:13 -0700

    added email property

--- api/migrations/001_create_users.py
+++ api/migrations/001_create_users.py
@@ -6,6 +6,7 @@ steps = [
             id SERIAL PRIMARY KEY NOT NULL,
             username VARCHAR(100) NOT NULL UNIQUE,
             password VARCHAR(256) NOT NULL,
+            email VARCHAR(100) NOT NULL,
             first_name VARCHAR(50),
             last_name VARCHAR(50),
             avatar_url VARCHAR(300),
@@ -21,10 +22,10 @@ steps = [
         # "Up" SQL statement
         """
         INSERT INTO users VALUES
-            (1, 'Tyler99', '123', 'Tyler', 'Primavera', null, 'A very nice guy.'),
-            (2, 'John93', '123', 'John', 'Gray', null, 'A very nice guy.'),
-            (3, 'Vinny2010', '123', 'Vinny', 'Vitiritto', null, 'A very mean guy.'),
-            (4, 'Judah44', '123', 'Judah', 'Viggers', null, 'A very nice guy.');
+            (1, 'Tyler99', '123', 'primaveradvm@gmail.com', 'Tyler', 'Primavera', null, 'A very nice guy.'),
+            (2, 'John93', '123', 'johnthomasgray1@gmail.com', 'John', 'Gray', null, 'A very old guy.'),
+            (3, 'Vinny2010', '123', 'vvitiritto@yahoo.com', 'Vinny', 'Vitiritto', null, 'A very mean guy.'),
+            (4, 'Judah44', '123', 'jmlv01@icloud.com', 'Judah', 'Viggers', null, 'A very young guy.');
         """,
         # "Down" SQL statement
         """
