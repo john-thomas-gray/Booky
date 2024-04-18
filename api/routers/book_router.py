b5257cc9fcc460d7672021b63c39b8e7b13c7285
Judah <jmlv01@icloud.com>
2024-04-17 17:51:18 -0700

    deletebook2

--- api/routers/book_router.py
+++ api/routers/book_router.py
@@ -2,39 +2,38 @@
 Book API Router
 """
 
-from fastapi import (
-    Depends,
-    Request,
-    Response,
-    HTTPException,
-    status,
-    APIRouter,
-)
-from queries.book_queries import (
-  BookQueries,
-)
-
+from fastapi import APIRouter, Depends, Request, Response, HTTPException
+from queries.book_queries import BookQueries
 from models.books import BookRequest, BookResponse
 
-router = APIRouter(tags=["Books"], prefix="/api/books")
+router = APIRouter(tags=["Book"], prefix="/api/book")
 
-@router.post("/")
+@router.post("/book")
 async def create_book(
-  new_book: BookRequest,
-  request: Request,
-  response: Response,
-  queries: BookQueries = Depends(),
+    new_book: BookRequest,
+    queries: BookQueries = Depends()
 ) -> BookResponse:
 
-  book = queries.create_book(
-    new_book.title,
-    new_book.author,
-    new_book.page_count,
-    new_book.genre,
-    new_book.publisher,
-    new_book.publication_date,
-    new_book.synopsis,
-    new_book.cover_img_url,
-  )
-  book_out = BookResponse(**book.model_dump())
-  return book_out
+    book = queries.create_book(
+        new_book.title,
+        new_book.author,
+        new_book.page_count,
+        new_book.genre,
+        new_book.publisher,
+        new_book.publication_date,
+        new_book.synopsis,
+        new_book.cover_img_url,
+    )
+    book_out = BookResponse(**book)
+    return book_out
+
+@router.delete("/book/{book_id}", response_model=BookResponse)
+async def delete_book(
+    book_id: int,
+    queries: BookQueries = Depends()
+) -> BookResponse:
+    deleted_book = queries.delete_book(book_id)
+    if deleted_book:
+        return deleted_book
+    else:
+        raise HTTPException(status_code=404, detail="Book not found")
