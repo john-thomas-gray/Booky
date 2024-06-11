b432f9b07ca5dbbb1636ed974a35a75a73a576a8
Vinny Vitiritto <vvitiritto@yahoo.com>
2024-06-11 23:12:54 +0000

    Css styling

--- ghi/src/services/authService.js
+++ ghi/src/services/authService.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { tryFetch } from '../utils'
 
-export const baseUrl = 'https://bookingforbooky.com'
+export const baseUrl = 'https://www.bookingforbooky.com'
 if (!baseUrl) {
     throw new Error('VITE_API_HOST was not defined')
 }
